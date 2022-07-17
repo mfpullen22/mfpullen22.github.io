d086a60b5cf1f020b3189868aebf2b2345e61f6b
Matthew Pullen <matthew.pullen@gmail.com>
2022-07-17 17:47:21 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Just a placeholder while full site is under construction!
+        </p>
+ 
+      </header>
+    </div>
+  );
+}
+
+export default App;
